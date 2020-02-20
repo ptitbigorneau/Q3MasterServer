f64ceef6c0a0665c38af3033dc446b13bba5d5c4
PtitBigorneau <steph_k29@hotmail.fr>
2020-02-20 22:04:35 +0100

    python 3

--- q3masterserver.py
+++ q3masterserver.py
@@ -1,5 +1,5 @@
-# Q3MasterServer
-# Copyright (C) 2015 PtitBigorneau
+# Q3MasterServer  (PYTHON 3)
+# Copyright (C) 2015-2020 PtitBigorneau
 #
 # PtitBigorneau - www.ptitbigorneau.fr
 #
@@ -12,67 +12,69 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
 __author__  = 'PtitBigorneau'
-__version__ = '1.0'
+__version__ = '2'
+#########################################################################################################
+import sys
 
+if sys.version_info < (3,):
+    raise SystemExit("Sorry, requires Python 3, not Python 2.")
+#########################################################################################################
 import socket
-
-try:
-    from socket import inet_ntop as inet_ntop
-except ImportError:
-    from dns.inet import inet_ntop as inet_ntop
-
+    
 class Q3masterServer:
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, opt):
 
-        packet_prefix = '\xff' * 4
+        packet_prefix = bytes([0xff] * 4)
         responses = []
         self.serverslist = []
-	
+
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect((host, port)) 
+        s.connect((host, port))
         s.settimeout(2)
 
-        cmd = packet_prefix+'getservers  68 full empty demo'
-        
+        option = "getservers  68 %s"%opt
+
+        cmd = packet_prefix + option.encode()
         s.send(cmd)
 
         while True:
-    
+
             try:
-                response = s.recv(1395)
+                response = s.recv(2048)
             except:
+
                 break
-            
+
             if not response :
                 break
             else:
                 responses.append(response)
 
         for packet in responses:
-    
+
             index = 22
 
             while True:
 
                 if index+7>=len(packet):
                     break
-            
-                ip = inet_ntop(socket.AF_INET, packet[index+1:index+5])
-                port = 256*ord(packet[index+5]) + ord(packet[index+6])
+
+                ip = socket.inet_ntop(socket.AF_INET, packet[index+1:index+5])
+                port = 256*packet[index+5] + packet[index+6]
                 index+=7
-                server =  ip+':'+str(port) 
+                server =  ip+':'+str(port)
                 self.serverslist.append(server)
-         
+
             s.close()
-        
+
         self.ListServers()
 
     def ListServers(self):
@@ -81,7 +83,9 @@ class Q3masterServer:
 
 if __name__ == '__main__':
 
-    m = Q3masterServer("master.urbanterror.info", 27900)
+    m = Q3masterServer("master.urbanterror.info", 27900, "full empty")
     listservers = m.ListServers()
     for server in listservers:
-        print server
+        print(server)
+
+    print(len(listservers))
